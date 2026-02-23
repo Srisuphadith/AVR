dff5007108d5bf60c1bcaaa957aa692d1070615a
Fubuki <ninerattana2005@gmail.com>
2026-02-23 17:17:50 +0700

    add comment to code

--- HW2.c
+++ HW2.c
@@ -1,9 +1,12 @@
+// define CPU frequence internal RC Oscillator 8 MHz = 8,000,000 Hz
 #define F_CPU 8000000UL
 #include <avr/io.h>
 #include <util/delay.h>
 void main()
 {
+    //Input pull-up
     PORTD |= 0x03;
+    //Output LED
     DDRB |= 0x02;
     while (1)
     {
