fe8c0c26bbb5c4426b6a1cb8b2b711de69d4e8fa
Fubuki <ninerattana2005@gmail.com>
2026-02-23 10:13:43 +0700

    led blink

--- main.c
+++ main.c
@@ -2,21 +2,14 @@
 #include <avr/interrupt.h>
 #define F_CPU 16000000UL
 #include <util/delay.h>
-ISR(INT0_vect){
-    //Toggle LED at PD3
-	PORTD = (PORTD & ~0x08) | ((~PIND) & 0x08);
-}
 void main()
 {
-  //output led PD2
-  DDRD |= 0x08;
-  //Input pull-up at PD2
-  PORTD |= 0x04;
-  //falling edge mode at INT0
-  EICRA |= (1 << ISC01);
-  //Open interrupt at INT0(PD3)
-  EIMSK |= (1 << INT0);    
-  //Open Global intterrupt
-  sei();
+    DDRB |= 0x01;
+    while(1){
+        PORTB |= 0x01;
+        _delay_ms(1000);
+        PORTB &= ~(0x01);
+        _delay_ms(1000);
+    }
   
 }
